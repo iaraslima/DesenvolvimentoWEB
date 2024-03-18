821be1a12995f93c758b568d8c4c224db2d41647
Iara Lima <iara.sl2804@gmail.com>
2024-03-18 08:49:51 -0300

    HelloWord.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+// import HelloWorld from "./components/01HelloWorld/HelloWord";
+// import FuncoesInternas from "./components/02FuncoesInternas/FunçõesInternas";
+// import Loja from "./components/03Props/Loja";
+//import MeusDados from "./components/atividade00/02MeusDados";
+//import Temperatura from "./components/atividade00/03Temperatura";
+//import SistemaSolar from "./components/04Children/SistemaSolar"
+//import ComPai from "./components/05ComPaiEFilho/ComPai";
+//import Estado from "./components/06Estados/Estado";
+//import Pokemon from "./components/06Estados/Pokemon";
+//import {CompA} from "./components/07PropDrilling/PropDrilling";
+//import {CompA} from "./components/08Contexto/Contexto";
+import PokemonPrincipal from "./components/09PokemonContexto/PokemonPrincipal";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <PokemonPrincipal/>
     </div>
   );
 }
---
+++ src/components/01HelloWorld/HelloWord.js
@@ -0,0 +1,8 @@
+const HelloWorld = () =>
+    <div>
+        <h2>
+            Jefferson de Carvalho
+        </h2>
+    </div>
+
+export default HelloWorld
